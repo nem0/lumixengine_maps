4ff1fc369bb4c4a5b9cf4506a96cafac7b343cff
Mikulas Florek <mikulas.florek@gamedev.sk>
2021-02-26 20:19:59 +0100

    flatten polylines

--- src/editor/plugins.cpp
+++ src/editor/plugins.cpp
@@ -2055,24 +2055,34 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 		Vec2 a2;
 	};
 
-	void flattenQuad(const Vec2* points, float h0, float h1, const Terrain& terrain) {
+	void flattenQuad(const Vec2* points, float h0, float h1, const Terrain& terrain, float line_width, float boundary_width) {
 		u16* ptr = (u16*)terrain.m_heightmap->data.getMutableData();
 		const u32 w = terrain.m_heightmap->width;
 		u16* hm_data = (u16*)terrain.m_heightmap->getData();
 
+		const Vec2 c0 = (points[0] + points[1]) * 0.5f;
+		const Vec2 c1 = (points[2] + points[3]) * 0.5f;
+		const Vec2 dir = normalize(c1 - c0);
+		const Vec2 n(-dir.y, dir.x);
+
 		const Vec2 min = minimum(points[0], minimum(points[1], minimum(points[2], points[3])));
 		const Vec2 max = maximum(points[0], maximum(points[1], maximum(points[2], points[3])));
-		const float heights[] = { h0, h0, h1, h1 };
+		const float heights[] = {h0, h0, h1, h1};
 		const u32 h = u32(max.y) + 1 - u32(min.y);
-		for (u32 pixelY = u32(min.y + 0.5f); pixelY < u32(max.y + 0.5f); ++pixelY) {
-			Vec2 nodeXY[256];
+
+		const i32 from_y = clamp(i32(min.y + 0.5f), 0, terrain.m_heightmap->height);
+		const i32 to_y = clamp(i32(max.y + 0.5f), 0, terrain.m_heightmap->height);
+		for (i32 pixelY = from_y; pixelY < to_y; ++pixelY) {
+			Vec2 nodeXY[4];
 			u32 nodes = 0;
+			const float y = pixelY + 0.5f;
 			for (i32 i = 0; i < 4; i++) {
-				if (points[i].y < (float)pixelY && points[(i + 1) % 4].y >= (float)pixelY ||
-					points[(i + 1) % 4].y < (float)pixelY && points[i].y >= (float)pixelY)
-				{
-					nodeXY[nodes].x = (points[i].x + (pixelY - points[i].y) / (points[(i + 1) % 4].y - points[i].y) * (points[(i + 1) % 4].x - points[i].x));
-					nodeXY[nodes].y = (heights[i] + (pixelY - points[i].y) / (points[(i + 1) % 4].y - points[i].y) * (heights[(i + 1) % 4] - heights[i]));
+				const float y0 = points[i].y;
+				const float y1 = points[(i + 1) % 4].y;
+				if (y0 < y && y1 >= y || y1 < y && y0 >= y) {
+					const float t = (y - y0) / (y1 - y0);
+					nodeXY[nodes].x = lerp(points[i].x, points[(i + 1) % 4].x, t);
+					nodeXY[nodes].y = lerp(heights[i], heights[(i + 1) % 4], t);
 					++nodes;
 				}
 			}
@@ -2080,67 +2090,56 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 			qsort(nodeXY, nodes, sizeof(nodeXY[0]), [](const void* a, const void* b){ 
 				const Vec2 m = *(const Vec2*)a;
 				const Vec2 n = *(const Vec2*)b;
-				return m.y == n.y ? 0 : (m.y < n.y ? -1 : 1); 
+				return m.x == n.x ? 0 : (m.x < n.x ? -1 : 1); 
 			});
-			if (nodes == 1) continue;
+			if ((nodes & 1) != 0) {
+				ASSERT(false);
+				logError("nodes == 1 ", points[0].x, ", ", points[0].y, " - ", points[2].x, ", ", points[2].y);
+				continue;
+			}
 
 			for (u32 i = 0; i < nodes; i += 2) {
-				nodeXY[i].x = clamp(nodeXY[i].x, 0.f, (float)w);
-				nodeXY[i + 1].x = clamp(nodeXY[i + 1].x, 0.f, (float)w);
-				for (i32 pixelX = i32(nodeXY[i].x + 0.5f); pixelX < i32(nodeXY[i + 1].x + 0.5f); ++pixelX) {
+				const i32 from = i32(clamp(nodeXY[i].x, 0.f, (float)w) + 0.5f);
+				const i32 to = i32(clamp(nodeXY[i + 1].x, 0.f, (float)w) + 0.5f);
+				const float rcp_xd = 1.f / (nodeXY[i + 1].x - nodeXY[i].x);
+				for (i32 pixelX = from; pixelX < to; ++pixelX) {
+					const float x = pixelX + 0.5f;
 					u16& v = hm_data[pixelX + pixelY * w];
-					
-					const float t = (pixelX - nodeXY[i].x) / (nodeXY[i + 1].x - nodeXY[i].x);
-					const float h = nodeXY[i].y * (1 - t) + nodeXY[i + 1].y * t;
-					v = u16(h / terrain.m_scale.y * 0xffFF);
+					const float t = (x - nodeXY[i].x) * rcp_xd;
+					const float h = lerp(nodeXY[i].y, nodeXY[i + 1].y, t);
+					const Vec2 p(x, y);
+					const float center_dist = abs(dot(p - c0, n));
+					if (center_dist < line_width * 0.5f) {
+						v = u16(h / terrain.m_scale.y * 0xffFF);
+					}
+					else {
+						const float h_orig = v * terrain.m_scale.y / 0xffFF;
+						const float h_lerp_t = clamp((center_dist - line_width * 0.5f) / boundary_width, 0.f, 1.f);
+						const float h_lerped = lerp(h, h_orig, h_lerp_t);
+						v = u16(h_lerped / terrain.m_scale.y * 0xffFF);
+					}
 				}
 			}
 		}
-		/*
-		auto set = [&](i32 i, i32 j){
-			const Vec2 p = Vec2((float)i, (float)j);
-			float t = dot((p - l0), normalize(l1 - l0)) / length(l1 - l0);
-			t = clamp(t, 0.f, 1.f);
-			ptr[i + j * stride] = u16(lerp(a, b, t).y / terrain.m_scale.y * 0xffff);
-		};
-
-		Quad2D l(l0, l1, line_width * 0.5f / terrain.m_scale.x);
-		for (i32 j = from.y; j <= to.y; ++j) {
-			for (i32 i = from.x; i <= to.x; ++i) {
-				Quad2D ij(i, j);
-				if (!ij.intersect(l)) continue;
-
-				Tri2D t0, t1;
-				ij.getTris(t0, t1);
-				set(i, j);
-				set(i + 1, j + 1);
-				set(i, j + 1);
-				set(i + 1, j);
-			}
-		}*/
 	}
 
-	void flattenLine(const DVec3& prev, const DVec3& a, const DVec3& b, const DVec3& next, const Terrain& terrain, float line_width) {
+	void flattenLine(const DVec3& prev, const DVec3& a, const DVec3& b, const DVec3& next, const Terrain& terrain, float line_width, float boundary_width) {
 		ASSERT(terrain.m_heightmap->format == gpu::TextureFormat::R16);
 		
-		/*if (squaredLength((b - a).xz()) > 100) {
+		if (squaredLength((b - a).xz()) > 9) {
 			DVec3 mid = (a + b) * 0.5f;
 			mid.y = terrain.getHeight((float)mid.x, (float)mid.z);
-			flattenLine(prev, a, mid, b, terrain, line_width);
-			flattenLine(a, mid, b, next, terrain, line_width);
+			flattenLine(prev, a, mid, b, terrain, line_width, boundary_width);
+			flattenLine(a, mid, b, next, terrain, line_width, boundary_width);
 			return;
-		}*/
+		}
 
 		const Vec2 a2d = Vec3(a).xz();
 		const Vec2 b2d = Vec3(b).xz();
 		const Vec2 prev2d = Vec3(prev).xz();
 		const Vec2 next2d = Vec3(next).xz();
 		const Vec2 dir = b2d - a2d;
-		Vec2 n0 = a2d - prev2d;
-		if (squaredLength(n0) < 1e-3) {
-			n0 = dir;
-		}
-		n0 = normalize(Vec2(n0.y, -n0.x));
+		const Vec2 n0 = normalize(Vec2(dir.y, -dir.x));
 
 		Vec2 n1 = next2d - b2d;
 		if (squaredLength(n1) < 1e-3) {
@@ -2148,19 +2147,22 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 		}
 		n1 = normalize(Vec2(n1.y, -n1.x));
 
-		Vec2 points[] = { a2d - n0 * line_width * 0.5f
-			, a2d + n0 * line_width * 0.5f
-			, b2d + n1 * line_width * 0.5f
-			, b2d - n1 * line_width * 0.5f
+		const float half_size = line_width * 0.5f + boundary_width;
+
+		Vec2 points[] = { a2d - n0 * half_size
+			, a2d + n0 * half_size
+			, b2d + n1 * half_size
+			, b2d - n1 * half_size
 		};
 
+		ASSERT(terrain.m_heightmap->width == terrain.m_heightmap->height);
+		const float s = terrain.m_heightmap->height / m_osm_parser.m_scale;
 		for (int i = 0; i < 4; ++i) {
-			points[i].x = points[i].x / m_osm_parser.m_scale * terrain.m_heightmap->width;
-			points[i].y = points[i].y / m_osm_parser.m_scale * terrain.m_heightmap->height;
+			points[i].x = points[i].x * s;
+			points[i].y = points[i].y * s;
 		}
 
-		flattenQuad(points, (float)a.y, (float)b.y, terrain);
-
+		flattenQuad(points, (float)a.y, (float)b.y, terrain, line_width * s, boundary_width * s);
 	}
 
 	void flattenPolylines(lua_State* L) {
@@ -2172,8 +2174,10 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 		if (!terrain->m_heightmap) luaL_error(L, "heightmap missing");
 		if (!terrain->m_heightmap->isReady()) luaL_error(L, "heightmap is not ready");
 
-		float line_width = 10.f;
+		float line_width = 3.f;
+		float boundary_width = 3.f;
 		LuaWrapper::getOptionalField(L, 1, "line_width", &line_width);
+		LuaWrapper::getOptionalField(L, 1, "boundary_width", &boundary_width);
 
 		for (pugi::xml_node& w : m_osm_parser.m_ways) {
 			if (!OSMParser::hasTag(w, def.key, def.value)) continue;
@@ -2187,7 +2191,8 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 					polyline[i + 1],
 					polyline[i + 2 < polyline.size() ? i + 2 : i + 1],
 					*terrain,
-					line_width);
+					line_width,
+					boundary_width);
 			}
 		}
 		terrain->m_heightmap->onDataUpdated(0, 0, terrain->m_heightmap->width, terrain->m_heightmap->height);
@@ -2501,7 +2506,7 @@ struct MapsPlugin final : public StudioApp::GUIPlugin
 			"cemetery\0landuse",
 			"reservoir\0landuse",
 			"water\0natural",
-			"\0building"
+			"\0building",
 			"\0highway",
 			"footway\0highway",
 			"track\0highway",
